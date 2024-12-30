46138946d35bd74e631580ccd7e49a6b19b4b000
Snehasish Mandal <mandalsneh97@gmail.com>
2024-12-30 21:47:32 +0530

    Updated the Urls.py

--- website/email_client/urls.py
+++ website/email_client/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SendEmailView, home
 
 urlpatterns = [
-    path('send', SendEmailView.as_view(), name='email_client'),
+    path('send/', SendEmailView.as_view(), name='email_client'),
     path('', home, name='home')
 ]
