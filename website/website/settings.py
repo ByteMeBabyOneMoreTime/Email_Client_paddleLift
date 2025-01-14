11b2a36bc1f041c1f37a7ed07f20b43a29cf1a99
mayankkumargupta1 <mayank0evil0side@gmail.com>
2025-01-14 20:00:39 +0530

    Disabled Debug

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-seplha@i0&f@u$(08r9ezc*ttg)wgt132cz-b65zpf2rb+a4s-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
